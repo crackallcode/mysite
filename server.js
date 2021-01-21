304f7c28f3cc4e55a836f4bf7a5711acca4b038c
Ryan <ryan@Ryans-MacBook-Pro.local>
2021-01-21 05:57:18 -0600

    second draft

--- server.js
+++ server.js
@@ -19,18 +19,26 @@ app.use(express.json());
 // });
 
 // The Server
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+app.use('/css/style.css', express.static(__dirname + 'public/css/style.css'))
+app.use('/css/style.css', express.static(__dirname + 'public/img'))
+
+
 
 app.get("/", function (req, res){
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/views/index.html")
 });
 
 app.get("/", function (req, res){
-    res.sendFile(__dirname + "/css/style.css")
+    res.sendFile(path.join(__dirname + "/style.css"))
 });
 
 app.get("/", function (req, res){
-    res.sendFile(__dirname + "/main.js")
+    res.sendFile(path.join(__dirname + "/../public/css/style.css"))
+}); 
+
+app.get("/", function (req, res){
+    res.sendFile(__dirname + ("/../main.js"))
 });
 
 app.listen(PORT, () => log("server is running "));
